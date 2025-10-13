cb284e69c605e361c58dea0782a9318f6ee0e2d2
Javier143322 <huhdnuhdl@gmail.com>
2025-10-13 02:57:48 -0500

    Actualizar fxmanifest.lua

--- esx_cocinacasera/fxmanifest.lua
+++ esx_cocinacasera/fxmanifest.lua
@@ -1,1 +1,21 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+description 'Sistema de Cocina Casera - Construcción Paso a Paso'
+version '1.0.0'
+
+-- Dependencias: Aseguramos que ESX esté cargado
+dependencies {
+    'es_extended'
+}
+
+client_scripts {
+    '@es_extended/config.lua',
+    'client.lua' -- Aquí estará la lógica de interacción y menú
+}
+
+server_scripts {
+    '@es_extended/config.lua',
+    'server.lua' -- Aquí estará la lógica de inventario y seguridad
+}
 
